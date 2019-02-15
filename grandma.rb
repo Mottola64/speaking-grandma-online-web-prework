72ebe0f80dde85c232a7f71f661fae03456cdb56
Michael Mottola <mottola64@gmail.com>
2019-02-15 03:27:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,18 @@
+def speak_to_grandma
+  if "Hello"==upcase
+    puts "HUH?! SPEAK UP, SONNY!"
+  elsif ""==upcase
+  puts "NO, NOT SINCE 1938!"
+else "I LOVE YOU GRANDMA!
+  puts 'I LOVE YOU TOO PUMPKIN!'
+end
+
+
+
 # Write a speak_to_grandma method.
+
+
+
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
